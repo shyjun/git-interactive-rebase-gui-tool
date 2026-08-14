15f57f64e425c3de30661d70c4a7c52a2a07f4cc
shyjun <n.shyju@gmail.com>
2026-08-14 16:53:34 +0530

    move from here. Move diff search tests to tests/ directory

--- tests/test_diff_search.py
+++ tests/test_diff_search.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 import os
 
-sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 from lib.app_window import _diff_search_matches
 
 
