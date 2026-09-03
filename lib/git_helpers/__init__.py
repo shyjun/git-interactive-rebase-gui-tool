6b777b23f001b1bcae214e39294479c6ce5fbc39
shyjun <n.shyju@gmail.com>
2026-09-03 23:11:59 +0530

    Fix import: add missing get_file_stats_between export

--- lib/git_helpers/__init__.py
+++ lib/git_helpers/__init__.py
@@ -56,6 +56,7 @@ from .diffs import (
     get_merge_base,
     get_diff_between,
     get_files_between,
+    get_file_stats_between,
     get_file_diff_between,
     get_unstaged_diff,
     get_unstaged_file_stats,
